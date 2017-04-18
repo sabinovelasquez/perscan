34d7c4ded566623d4388b496566ca5bd0941e16d
Sabino Velasquez <sabinovelasquez@gmail.com>
2017-04-18 15:41:30 -0300

    svg mask

--- index.android.js
+++ index.android.js
@@ -1,52 +1,72 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+'use strict';
 
 import React, { Component } from 'react';
 import {
+  ActivityIndicator,
   AppRegistry,
+  Dimensions,
   StyleSheet,
   Text,
-  View
+  Image,
+  TouchableOpacity,
+  TouchableHighlight,
+  View,
+  StatusBar
 } from 'react-native';
+import Camera from 'react-native-camera';
 
 export default class perscan extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      capType: Camera.constants.Type.back
+    };
+  }
+  componentDidMount() {
+    this.setState({
+      title: 'PERSCAN'
+    });
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+        <View style={styles.bgContainer}>
+          <Image source={require('./assets/bg.png')} style={styles.backgroundImage}>
+            <Text style={styles.fontLight}>
+              {this.state.title}
+            </Text>
+          </Image>
+        </View>
       </View>
     );
   }
+  /////render Ends
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
+    alignItems: 'center'
+  },
+  bgContainer: {
+    height: '40%',
+    width: '100%',
+    borderBottomLeftRadius: 50,
+    borderBottomRightRadius: 50,
+    overflow: 'hidden',
+  },
+  backgroundImage: {
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    resizeMode: 'cover',
+    height: '100%',
+    width: '100%'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  fontLight: {
+    fontFamily: 'Montserrat-Light',
+    fontSize: 36,
+    marginTop: '40%',
+    color: '#fff',
+    backgroundColor: 'transparent'
   },
 });
 
--- index.ios.js
+++ index.ios.js
@@ -7,18 +7,39 @@ import {
   Dimensions,
   StyleSheet,
   Text,
-  Image,
   TouchableOpacity,
   TouchableHighlight,
   View,
   StatusBar
 } from 'react-native';
+
+import Svg,{
+  Circle,
+  Ellipse,
+  G,
+  LinearGradient,
+  RadialGradient,
+  Line,
+  Image,
+  Path,
+  Polygon,
+  Polyline,
+  Rect,
+  Symbol,
+  ClipPath,
+  Use,
+  Defs,
+  Stop
+} from 'react-native-svg';
+
 import Camera from 'react-native-camera';
 
 export default class perscan extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      height: Dimensions.get('window').height,
+      width: Dimensions.get('window').width,
       capType: Camera.constants.Type.back
     };
   }
@@ -30,11 +51,34 @@ export default class perscan extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Image source={require('./assets/bg.png')} style={styles.backgroundImage}>
+        <Svg height={100} width={100}>
+          <Image
+            width="50%"
+            height="50%"
+            x="19"
+            y="19"
+            href={require('./assets/logo.png')}
+          />
+        </Svg>
+        <Svg height={this.state.height} width={this.state.width}>
+          <Defs>
+            <ClipPath id='clip'>
+              <Circle cx='50%' cy='100%' r='100%'/>
+            </ClipPath>
+          </Defs>
+          <Image
+            x="0"
+            y="0"
+            width="100%"
+            height="100%"
+            preserveAspectRatio="xMidYMid slice"
+            href={require('./assets/bg.png')}
+            clipPath="url(#clip)"
+          />
           <Text style={styles.fontLight}>
-            {this.state.title}
+              {this.state.title}
           </Text>
-        </Image>
+        </Svg>
       </View>
     );
   }
@@ -43,14 +87,13 @@ export default class perscan extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    backgroundColor: '#000'
   },
   backgroundImage: {
     alignItems: 'center',
     resizeMode: 'cover',
-    height: '100%',
-    width: '100%',
+    height: '10%',
+    width: '10%',
   },
   fontLight: {
     fontFamily: 'Montserrat-Light',
